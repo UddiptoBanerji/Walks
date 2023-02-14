80e1116912d3936d2c0223aceb2d2c4cb2d56cf7
uddipto.banerji <uddipto.banerji@accenture.com>
2023-02-14 11:15:14 +0530

    Migration Script and Code changes

---
+++ NZWalks/NZWalksAPI/Data/WalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalksAPI.Models;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Data
+{
+    public class WalksDbContext: DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
